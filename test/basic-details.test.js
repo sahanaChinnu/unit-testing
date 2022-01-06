023e45a1511de57ee455d22079e18784d3b3d0f3
Administrator <Administrator@EC2AMAZ-ACBI12F>
2022-01-06 13:54:58 +0000

    basic details unit tests

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -1,8 +1,17 @@
 import { html, fixture, expect } from '@open-wc/testing';
-import { stub } from 'sinon';
+// import { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
+
+const component = await fixture(html `<basic-details></basic-details>`);
+const button = el.shadowRoot.querySelectorAll('lion-button');
 
 describe('Basic details', () => {
   // Write test cases inside this block
   // refer basic-details.js files
+
+ it('component access', () => {
+   expect(component).to.be.accessible; });
+
+it('button ')
+
 });
