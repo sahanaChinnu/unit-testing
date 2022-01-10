f7a28aa9b1cbce24532de1955261f6ae98a06a6f
sahanaChinnu <43902132+sahanaChinnu@users.noreply.github.com>
2022-01-10 15:06:51 +0000

    Basic unit testing

--- test/customer-details.test.js
+++ test/customer-details.test.js
@@ -1,7 +1,17 @@
 import { html, fixture, expect } from '@open-wc/testing';
-import { stub } from 'sinon';
+import Sinon from 'sinon';
 import '../src/Customer/Customer-details.js';
 
+const component = await fixture(html `<customer-details></customer-details>`);
+const button = component.shadowRoot.querySelector('lion-button');
 describe('customer details', () => {
-  // Write test cases inside this block
+
+  it('component access', () => {
+    expect(component).to.be.accessible; });
+ 
+    it('button action', () => {
+     const action = Sinon.spy(component,"_toEmidetails")
+     button.click();
+     expect(action.calledOnce).to.be.false; });
+
 });
