b0b2df0b236d6a258adc1714b97af5383502a0b1
sahanaChinnu <43902132+sahanaChinnu@users.noreply.github.com>
2022-01-10 15:09:04 +0000

    Basic unit tests

--- test/loan-application.test.js
+++ test/loan-application.test.js
@@ -2,6 +2,11 @@ import { html, fixture, expect } from '@open-wc/testing';
 
 import '../loan-application.js';
 
+
+const component = await fixture(html `<loan-application></loan-application>`);
+const button = component.shadowRoot.querySelector('lion-button');
+
 describe('LoanApplication', () => {
-  // Write test cases inside this block
+  it('component access', () => {
+    expect(component).to.be.accessible; });
 });
