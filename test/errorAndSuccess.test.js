518ed9dfb50fb6f3e3feceb26a25e2bd699ae405
sahanaChinnu <43902132+sahanaChinnu@users.noreply.github.com>
2022-01-10 15:07:40 +0000

    Update errorAndSuccess.test.js

--- test/errorAndSuccess.test.js
+++ test/errorAndSuccess.test.js
@@ -1,12 +1,31 @@
 import { html, fixture, expect } from '@open-wc/testing';
-// import { stub } from 'sinon';
+import Sinon from 'sinon';
 import '../src/SuccessAndError/Success.js';
 import '../src/SuccessAndError/Error.js';
 
+const errorComponent = await fixture(html `<loan-error></loan-error>`);
+const errorButton = errorComponent.shadowRoot.querySelector('lion-button');
+const successComponent = await fixture(html `<loan-success></loan-success>`);
+const successbutton = successComponent.shadowRoot.querySelector('lion-button');
+
 describe('Success screen ', () => {
-  // Write test cases inside this block
+
+  it('component access', () => {
+    expect(successComponent).to.be.accessible; });
+ 
+    it('button action', () => {
+     const action = Sinon.spy(successComponent,"_toHome")
+     successbutton.click();
+     expect(action.calledOnce).to.be.false; });
+
 });
 
 describe('error screen', () => {
-  // Write test cases inside this block
+  it('component access', () => {
+    expect(errorComponent).to.be.accessible; });
+ 
+    it('button action', () => {
+     const action = Sinon.spy(errorComponent,"_toHome")
+     errorButton.click();
+     expect(action.calledOnce).to.be.false; });
 });
