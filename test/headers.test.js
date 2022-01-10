4ab0c4b5ce226dfdb0113179c1f09a832aa5346b
sahanaChinnu <43902132+sahanaChinnu@users.noreply.github.com>
2022-01-10 15:08:22 +0000

    Basic unit tests

--- test/headers.test.js
+++ test/headers.test.js
@@ -1,7 +1,19 @@
 import { html, fixture, expect } from '@open-wc/testing';
-import { stub } from 'sinon';
+import Sinon from 'sinon';
 import '../src/header/Header.js';
 
+const component = await fixture(html `<loan-header></loan-header>`);
+const button = component.shadowRoot.querySelector('button');
+
 describe('loan-header', () => {
-  // Write test cases inside this block
+  it('component access', () => {
+    expect(component).to.be.accessible; });
+ 
+    it('button action', () => {
+     const action = Sinon.spy(component,"localeChanged")
+     button.click();
+     expect(action.calledOnce).to.be.true; });
+     
+    
+    
 });
