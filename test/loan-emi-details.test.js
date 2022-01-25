683ecb3014be49b5575370a027bc314a9c83db89
sahanaChinnu <43902132+sahanaChinnu@users.noreply.github.com>
2022-01-25 08:40:35 +0000

    Fixes

--- test/loan-emi-details.test.js
+++ test/loan-emi-details.test.js
@@ -1,20 +1,22 @@
 import { html, fixture, expect } from '@open-wc/testing';
 import '../src/LoanEMIDetails/LoanEMIDetails.js';
+import Sinon from 'sinon';
 
-// const component = await fixture(html `<loanemi-details></loanemi-details>`);
-// const button = component.shadowRoot.querySelectorAll('lion-button');
+ const component = await fixture(html `<loanemi-details></loanemi-details>`);
+ const button = component.shadowRoot.querySelectorAll('lion-button');
 
-xdescribe('Loan EMI details', () => {
-  xit('component access', () => {
+describe('Loan EMI details', () => {
+
+  it('component access', () => {
     expect(component).to.be.accessible; });
  
     it('button action', () => {
-     const action = Sinon.spy(component,"_toBasicDetails")
+       const action = Sinon.spy(component,"_toBasicDetails")
      button[0].click();
-     expect(action.calledOnce).to.be.false; });
+     expect(action.calledOnce).to.be.true; });
 
      it('button action', () => {
-      const action = Sinon.spy(component,"_toCustomer")
+            const action = Sinon.spy(component,"_toCustomer")
       button[1].click();
-      expect(action.calledOnce).to.be.false; });
+      expect(action.calledOnce).to.be.true; });
  });
